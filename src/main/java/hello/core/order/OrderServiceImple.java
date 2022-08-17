9db241778a77c917c77cc71dffb3800742ae355e
dong c <dchoi1211@gmail.com>
2022-08-17 16:21:18 +0900

    AppConfig

--- src/main/java/hello/core/order/OrderServiceImple.java
+++ src/main/java/hello/core/order/OrderServiceImple.java
@@ -1,7 +1,6 @@
-package hello.core.order;
+package hello.core.discount.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +8,16 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImple implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+   /* private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    --> 셀제 코드를 보면 DiscountPlicy와 구현체를 모두 의존하고 있음
+    DIP를 위반
+    이런 경우는 정책을 바꿀 때, OrderServicelmple도 변경을 해주어야 한다.
+    */
+
+    private DiscountPolicy discountPolicy;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
